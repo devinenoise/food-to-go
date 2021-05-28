ff23d9f68adf881f9a0e5326e1bbfcfceae7b36d
Kyle Devine <devinenoise@gmail.com>
2021-05-28 12:54:55 -0700

    displaying meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { DUMMY_MEALS } from '../../dummy-meals';
 import classes from './AvailableMeals.module.css';
-import MealItem from './MealItem';
+import MealItem from './MealItem/MealItem';
+import Card from '../UI/Card';
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map(meal => {
     return (
       <li>
         <MealItem
+          key={meal.id}
           name={meal.name}
           description={meal.description}
           price={meal.price}
@@ -18,7 +20,9 @@ const AvailableMeals = () => {
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
