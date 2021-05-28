83adf34d12525b74dd2ae267676bc5f03a0baed7
Kyle Devine <devinenoise@gmail.com>
2021-05-28 12:36:50 -0700

    main meals component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { DUMMY_MEALS } from '../../dummy-meals';
+import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
-  return <div></div>;
+  const mealsList = DUMMY_MEALS.map(meal => {
+    return <li>{meal.name}</li>;
+  });
+
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
 };
 
 export default AvailableMeals;
